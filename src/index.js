49ae6d61e3b7cece95ee9c3f0d0535840e2d3cfe
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-10-31 13:29:12 +0530

    The compilation is successful

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+import store from './reducers';
 
+import VideoSelectScreen from './screens/VideoSelectScreen';
+import ConvertScreen from './screens/ConvertScreen';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route path="/convert" component={ConvertScreen} />
+            <Route path="/" component={VideoSelectScreen} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>,
+    document.getElementById('root')
 );
 
