22e74c9277a7a1ec71fef7becd6af051fd8ca76c
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-10-31 12:58:32 +0530

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  });
+
+  require('@electron/remote/main').initialize();
+  require('@electron/remote/main').enable(mainWindow.webContents);
+
+  mainWindow.loadURL(isDev ?
+      'http://localhost:3000' :
+      `file://${path.join(__dirname, '../build/index.html')}`
+  );
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', function () {
+  // On OS-X applications stay open even when all windows are closed.
+  // User can quit from Menu
+  // if (process.platform !== 'darwin') {
+  //   app.quit();
+  // }
+  app.quit();
+});
+
+app.on('activate', function () {
+  // On OS0X its common to recreate a window in the app the dock icon is clied
+  // and there are no other windows open
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+})
+
